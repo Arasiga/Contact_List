e4b432aca2b2c0998cfcb9f62d387fd6f3cf1db8
Alex Rasiga <alex.rasiga@gmail.com>
2016-03-16 20:13:24 +0000

    modified

--- contact_list.rb
+++ contact_list.rb
@@ -2,62 +2,6 @@ require 'pry'
 require 'pg'
 require 'active_record'
 
-# class Contact
-
-#   attr_accessor :name, :email
-  
-#   def initialize(name, email)
-    
-#     @name = name
-#     @email = email
-#   end
-
-#   class << self
-
-#     def connection(string, *value)
-#       conn = PG.connect(
-#         host: 'localhost',
-#         dbname: 'contact',
-#         user: 'development',
-#         password: 'development'
-#       )
-#       res = conn.exec(string, value)
-#       conn.close
-#       res
-#     end
-
-#     def all 
-#       result = connection('SELECT * FROM contacts;')
-#       result.to_a
-#     end
-
-#    def save(name, email)
-#       result = connection('INSERT INTO contacts (name, email) VALUES($1, $2)', name, email)
-#    end  
-    
-#     def find(id)
-#       result = connection('SELECT * FROM contacts WHERE id = $1::int', id)
-#       result.to_a
-#     end
-  
-#     def search(term)      
-#       result = connection('SELECT * FROM contacts WHERE UPPER(name) LIKE UPPER($1) OR UPPER(name) LIKE UPPER($1)', '%'+term+'%') 
-#       result.to_a                
-#     end
-
-#     def update(name, email, name_id)
-#       the_contact = Contact.find(name_id)
-#       number = the_contact[0]["id"].to_i
-#       result = connection('UPDATE contacts SET name = $1, email = $2 WHERE id = $3', name, email, name_id)
-#     end
-
-#     def delete(id)
-#       connection('DELETE FROM contacts WHERE id = $1;', id)
-#     end
-
-#   end
-# end
-
 ActiveRecord::Base.establish_connection(
   adapter: 'postgresql',
   database: 'contact',
@@ -73,7 +17,7 @@ ActiveRecord::Base.establish_connection(
 
 
 class Contact < ActiveRecord::Base
-  
+
 end
 
 
